3fdc96d491cb40da8b8a5bb1a3548e780ab5116c
r-ush <aarush.bhatt@gmail.com>
2020-09-06 13:51:50 +0530

    added esc key binding to profile page

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,33 +1,24 @@
 import React from "react";
-import { Typography } from "antd";
+import { Layout } from "antd";
 import Buttons from "../../components/button/button";
 import "./main.css";
-// import logo from "../../images/enigma.svg";
+import logo from "../../images/enigma.svg";
 
 const MainPage = () => {
     return (
-        <div className="page main-page">
-            {/* <div className="heading-group">
+        <Layout className="page">
+            <div className="container-box">
+                <div className="heading-group">
                     <img src={logo} alt="enigma.svg" />
-                </div> */}
-            <div className="login-header">
-                <div className="login-header-background">
-                    10101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100110010010101101010100101001100100101011010101001010011001001010110101010010100
                 </div>
-                <div className="login-heading">
-                    <Typography className="login-title">ENIGMA</Typography>
-                    <Typography className="login-subtitle">
-                        online cryptic hunt
-                    </Typography>
+                <div className="buttons">
+                    <Buttons redirect="Questions" name="Questions" />
+                    <Buttons redirect="profile" name="Profile" />
+                    <Buttons redirect="story" name="Story" />
+                    <Buttons redirect="leaderboard" name="Leaderboard" />
                 </div>
             </div>
-            <div className="main-buttons">
-                <Buttons redirect="Questions" name="Questions" />
-                <Buttons redirect="profile" name="Profile" />
-                <Buttons redirect="story" name="Story" />
-                <Buttons redirect="leaderboard" name="Leaderboard" />
-            </div>
-        </div>
+        </Layout>
     );
 };
 
