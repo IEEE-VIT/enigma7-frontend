01923ac68777865cb51a79792b6d29aea7b3eae5
ashikka <ashikagupta28@gmail.com>
2020-09-22 17:54:52 +0530

    fix: Added keybindings

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Layout } from "antd";
 import Buttons from "../../components/button/button";
 import "./main.css";
 import logo from "../../images/enigma.svg";
+import useKeyPress from "../../hooks/useKeyPress";
 
 const MainPage = () => {
+    const questions = useRef(null);
+    const profile = useRef(null);
+    const story = useRef(null);
+    const leaderboard = useRef(null);
+
+    useEffect(() => {
+        questions.current.focus();
+    }, []);
+
+    if (useKeyPress("ArrowDown")) {
+        profile.current.focus();
+        story.current.focus();
+        leaderboard.current.focus();
+    }
+
+    if (useKeyPress("ArrowUp")) {
+        questions.current.focus();
+    }
     return (
         <Layout className="page">
             <div className="container-box">
@@ -12,10 +31,19 @@ const MainPage = () => {
                     <img src={logo} alt="enigma.svg" />
                 </div>
                 <div className="buttons">
-                    <Buttons redirect="Questions" name="Questions" />
-                    <Buttons redirect="profile" name="Profile" />
-                    <Buttons redirect="story" name="Story" />
-                    <Buttons redirect="leaderboard" name="Leaderboard" />
+                    <Buttons
+                        ref={questions}
+                        redirect="Questions"
+                        name="Questions"
+                        autofocus
+                    />
+                    <Buttons ref={profile} redirect="profile" name="Profile" />
+                    <Buttons ref={story} redirect="story" name="Story" />
+                    <Buttons
+                        ref={leaderboard}
+                        redirect="leaderboard"
+                        name="Leaderboard"
+                    />
                 </div>
             </div>
         </Layout>
