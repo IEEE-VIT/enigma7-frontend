a4d6f79f07b6edf1b83ba776e5bf7d85a605f66e
r-ush <aarush.bhatt@gmail.com>
2020-11-21 22:07:41 +0530

    redeploy

--- src/pages/firstLogin/FirstLoginPage.jsx
+++ src/pages/firstLogin/FirstLoginPage.jsx
@@ -43,7 +43,6 @@ const FirstLogin = () => {
         // eslint-disable-next-line
     }, []);
 
-    //
     // const onFinish = (values) => {
     //     console.log(values);
     //     setError("error 404! no backend found");
