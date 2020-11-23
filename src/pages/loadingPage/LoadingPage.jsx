d74c50fd4b8e5ecac561863c40327e8043afd966
r-ush <aarush.bhatt@gmail.com>
2020-11-23 21:55:45 +0530

    added loader for first login

--- src/pages/loadingPage/LoadingPage.jsx
+++ src/pages/loadingPage/LoadingPage.jsx
@@ -1,7 +1,25 @@
+import { Typography } from "antd";
 import React from "react";
+import Loading from "../../images/loading.svg";
 
 const LoadingPage = () => {
-    return <div className="page">Loading</div>;
+    return (
+        <div
+            className="page"
+            style={{
+                width: "100%",
+                flexDirection: "column",
+                background: "#182F24",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <img style={{ width: "100%" }} src={Loading} alt="loader" />
+            <Typography.Title style={{ color: "#ffffff" }}>
+                Loading
+            </Typography.Title>
+        </div>
+    );
 };
 
 export default LoadingPage;
