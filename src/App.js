9a189292e0c97d476bab1212dacd86815d2fa7c0
ZiyanK <ziyankarmali786@gmail.com>
2020-12-07 00:07:57 +0530

    Added thank you page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Layout } from "antd";
-import LeaderBoardPage from "./pages/leaderboard/LeaderBoardPage";
+// import LeaderBoardPage from "./pages/leaderboard/LeaderBoardPage";
 // import TimerPage from "./pages/timer/TimerPage";
-import LoginPage from "./pages/login/LoginPage";
-import FirstLoginPage from "./pages/firstLogin/FirstLoginPage";
+// import LoginPage from "./pages/login/LoginPage";
+// import FirstLoginPage from "./pages/firstLogin/FirstLoginPage";
 import NotFound from "./pages/notFound/NotFound";
 // import Privacy from "./pages/privacy/Privacy";
-import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
+// import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
 import Partners from "./pages/partners/Partners";
 import ThankYouPage from "./pages/thankYou/ThankYouPage";
 
@@ -72,7 +72,7 @@ const App = () => {
                     path="/instructions"
                     component={InfoPage}
                 /> */}
-                <ProtectedRoute
+                {/* <ProtectedRoute
                     redirect="/"
                     exact
                     path="/leaderboard"
@@ -83,17 +83,12 @@ const App = () => {
                     exact
                     path="/first-login"
                     component={FirstLoginPage}
-                />
-                <ProtectedRoute
-                    redirect="/"
-                    exact
-                    path="/thank-you"
-                    component={ThankYouPage}
-                />
+                /> */}
+                <Route redirect="/" exact path="/" component={ThankYouPage} />
                 {/* <Route exact path="/download" component={DownloadApp} /> */}
                 <Route exact path="/partners" component={Partners} />
                 <Route path="/apple-app-site-association" onEnter={reload} />
-                <Route exact path="/" component={LoginPage} />
+                {/* <Route exact path="/" component={LoginPage} /> */}
                 {/* <Route path="/privacy" exact component={Privacy} /> */}
                 <Route component={NotFound} />
             </Switch>
